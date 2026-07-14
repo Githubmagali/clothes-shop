9a37ae59e83ceb90fbd8f5a9164c0edaa32edb86
Magali Fernandez <MagaliVictoria85068@gmail.com>
2026-07-14 20:36:56 -0300

    fix: actualizar js-yaml a versión sin vulnerabilidad

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,6 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import { CartProvider } from "../context/page";
 
-
 export default function RootLayout({ children }) {
   return (
     <html>
